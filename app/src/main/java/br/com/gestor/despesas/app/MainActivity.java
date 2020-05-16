f61b5933533ca808e9b658bc61362c32f4f92226
Cristierre Gomes Konrath <cristierre.konrath@dbserver.com.br>
2020-05-15 22:07:29 -0300

    Cria Layout e class Cadastro

--- app/src/main/java/br/com/gestor/despesas/app/MainActivity.java
+++ app/src/main/java/br/com/gestor/despesas/app/MainActivity.java
@@ -2,13 +2,38 @@ package br.com.gestor.despesas.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etNome, etSenha;
+    Button btnLogin, btnCadastrar;
+    private FirebaseAuth auth;
+    private FirebaseUser usuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNome = findViewById(R.id.etNome);
+        etSenha = findViewById(R.id.etSenha);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
